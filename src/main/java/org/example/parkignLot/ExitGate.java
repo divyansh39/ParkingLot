68fe71a86ac4bf055760ba5ce5aab456fafc292a
Divyanshi <divya28.kush@gmail.com>
2026-04-27 03:27:27 +0530

    added Exit gate chnages

--- src/main/java/org/example/parkignLot/ExitGate.java
+++ src/main/java/org/example/parkignLot/ExitGate.java
@@ -1,4 +1,31 @@
 package org.example.parkignLot;
 
+import org.example.entity.Ticket;
+import org.example.payment.Payment;
+import org.example.pricingStrategy.CostComputation;
+
 public class ExitGate {
+
+    private final CostComputation costComputation;
+
+    public  ExitGate(CostComputation costComputation){
+        this.costComputation=costComputation;
+    }
+
+    public void completeExit(ParkingBuilding parkingBuilding, Ticket ticket, Payment payment){
+        double amount=calculatePrice(ticket);
+        boolean success=payment.pay(amount);
+
+        if(!success){
+            throw  new RuntimeException("Payment Failed, Exit Denied");
+        }
+        parkingBuilding.release(ticket);
+        System.out.println("Exit Successful. Gate opened");
+
+    }
+
+    private double calculatePrice(Ticket ticket){
+        return costComputation.compute(ticket);
+    }
+
 }
